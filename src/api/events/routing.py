8a815d36163097b91a221956e5c006b21e5006c7
Seu Nome Novo <brunomitchell.ofc@gmail.com>
2026-04-09 11:12:05 +0200

    5 - Basic Data Types

---
+++ src/api/events/routing.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from.schema import EventSchema
+
+router = APIRouter()
+
+# /api/events/
+@router.get("/")
+def read_events():
+  # a bunch of items in a table
+  return {
+    "results": [1,2,3]
+  }
+
+@router.get("/{event_id}")
+def get_event(event_id: int) -> EventSchema:
+  # a single row
+    return {"id": event_id}
--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from typing import Union
 from fastapi import FastAPI
+from api.events import router as event_router
 
 app = FastAPI()
+app.include_router(event_router, prefix='/api/events')
 
 
 @app.get("/")
@@ -11,3 +14,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/healthz")
+def read_api_health():
+    return {"status": "ok"}
